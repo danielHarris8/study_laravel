c67af7571b007942dffe7011203a0ae7a8ebed2d
danielHarris8 <dh302758@gmail.com>
2021-01-26 05:26:48 -0500

    jwt practice

---
+++ laravel-jwt-auth/app/Http/Middleware/Authenticate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            // return route('login');
+            // return response()->json(['error'=>'token expired'],400);
+            // return abort(401,'token expired');
+        }
+    }
+
+
+
+}
